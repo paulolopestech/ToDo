aeb63e69386b5fda1c49966f4ef0e9635468a793
paulolopestech <phlj.junior@gmail.com>
2023-09-24 12:15:14 -0300

    :construction: update tasks

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,32 +1,37 @@
 import { Task } from "./types";
 
 export class Tasks {
-    public tasks: Array<Task> = [];
+  public tasks: Array<Task> = [];
 
-    validateCreateTask(task: Task) {
-        if(!task.title) return false;
-        if(typeof task.title !== "string") return false;
-        if(!task.description) return false;
-        if(typeof task.description !== "string") return false;
-        if(!task.status) return false;
-        if(typeof task.status !== "string") return false;
-        return true;
-      }
-      
-    createTask (task: Task) {
-      this.tasks.push(task);
-    }
+  validateTask(task: Task) {
+    if (!task.title) return false;
+    if (typeof task.title !== "string") return false;
+    if (!task.description) return false;
+    if (typeof task.description !== "string") return false;
+    if (!task.status) return false;
+    if (typeof task.status !== "string") return false;
+    return true;
+  }
 
-    listTasks(): Task[] {
-      return this.tasks;
+  createTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  listTasks(): Array<Task> {
+    return this.tasks;
+  }
+
+  update(index: number, task: Task): Task {
+    this.tasks[index] = task;
+    return this.tasks[index];
   }
 
   deleteTask(index: number) {
     if (index >= 0 && index < this.tasks.length) {
-        this.tasks.splice(index, 1);
-        return true;
+      this.tasks.splice(index, 1);
+      return true;
     }
     return false;
-}
+  }
 
 }
