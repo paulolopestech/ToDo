988019d25cfbbfd1c39c66d598e0b276cdc81b21
Pietro19 <piscardoso15@gmail.com>
2023-09-24 11:29:53 -0300

    add list update delete

--- src/app.cmd.ts
+++ src/app.cmd.ts
@@ -34,13 +34,62 @@ async function cmdApp() {
         if(taskIsValid) tasks.createTask(task);
         else console.log('Os dados que você inseriu são inválidos, tente novamente!');
         break;
-    
+      
+      case '2':  
+        console.log('=== Lista de Tarefas ===');
+        tasks.tasks.forEach((task, index) => {
+        console.log(`Tarefa ${index + 1}:`);
+        console.log(`Título: ${task.title}`);
+        console.log(`Descrição: ${task.description}`);
+        console.log(`Status: ${task.status}`);
+        console.log('-------------------');  });
+        break;
+
+      case '3':
+        console.log('=== Atualizar uma Tarefa ===');
+        const taskIndex = parseInt(await getUserInput('Digite o número da tarefa que deseja atualizar: '), 10) - 1;
+        if (isNaN(taskIndex) || taskIndex < 0 || taskIndex >= tasks.tasks.length) {
+            console.log('Índice de tarefa inválido.');
+        } else {
+             const updatedTask = tasks.tasks[taskIndex];
+
+            const updatedTitle = await getUserInput('Digite o novo título da tarefa: ');
+            if (updatedTitle) {
+                 updatedTask.title = updatedTitle;
+             }
+
+             const updatedDescription = await getUserInput('Digite a nova descrição da tarefa: ');
+            if (updatedDescription) {
+                updatedTask.description = updatedDescription;
+             }
+
+            const updatedStatus = await getUserInput('Digite o novo status da tarefa: ');
+            if (updatedStatus) {
+                updatedTask.status = updatedStatus;
+            }
+
+            console.log('Tarefa atualizada com sucesso.');
+        }
+        break;  
+
+        case '4':
+          console.log('=== Excluir uma Tarefa ===');
+          const taskIndexToDelete = parseInt(await getUserInput('Digite o número da tarefa que deseja excluir: '), 10) - 1;
+          if (isNaN(taskIndexToDelete) || taskIndexToDelete < 0 || taskIndexToDelete >= tasks.tasks.length) {
+              console.log('Índice de tarefa inválido.');
+          } else {
+              tasks.tasks.splice(taskIndexToDelete, 1);
+              console.log('Tarefa excluída com sucesso.');
+          }
+          break;
+
+
       default:
         console.log('Fechando aplicação');
         rl.close()
         return;
     }
-    console.clear();
+    //console.clear();
   }
 }
 
